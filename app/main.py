7b2ce6ce6f665fd982c38b4597b1f03e88f3455f
MinHee Kim <minhee.kim@ibm.com>
2023-08-09 23:03:03 +0900

    first commit

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import logging.config
+
+
+import os
+
+logger = logging.getLogger("productService")
+
+# Set logger name to project
+
+logger.info("START Application")
+
+
+# Define Fast api and description
+app = FastAPI(
+    title="Product service for Instana",
+    description="This is a template of python MSA used in Garage project.",
+    version="0.0.1",
+)
+
+
+
+# This path is for health check or test
+@app.get("/")
+async def root():
+    return {"Hello World :D"}
+
+
+@app.get("/test")
+async def test():
+    return {"Test endpoint"}
